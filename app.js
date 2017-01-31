f113ac200b10b9f2e328b8e8ee4de1849de33fd1
Richard Stanley <audstanley@gmail.com>
2017-01-30 23:07:18 -0800

    initial commit.

---
+++ app.js
@@ -0,0 +1,40 @@
+let schedule = require('node-schedule');
+let fetch = require('node-fetch');
+let push = require('pushover-notifications');
+
+let regex = /<a href="latest-v(\d{1,})/;
+let version = "";
+let p = new push({
+	user: "AAAAAAAAAAAAAAAAAAAAAAAAAA",	//user API key
+	token: "BBBBBBBBBBBBBBBBBBBBBBBBBB",	//token API key
+})
+
+let job = schedule.scheduleJob('*/20 * * * * *', function() {
+	console.log("this should be every ten minutes");
+	fetch('https://nodejs.org/dist').then(function(res) {
+		return res.text();
+	}).then(function(body) {
+		let a = body.split("\n").filter(getLatest).reverse()[0].match(regex)[1];
+		let msg = {
+			message: 'NodeJs has been updated to version: ' + a,
+			title: 'NodeJs Update'
+		}
+		if(version === "")  {
+			console.log("version check: " + a);
+			version = a;
+		}
+		else if(a !== version) {
+			p.send(msg, function(err, result) {if(err) throw err; console.log(result);})	
+		}
+	})
+
+
+})
+
+
+
+
+
+let getLatest = (str) => {
+	return str.match(regex);
+}
